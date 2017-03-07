4b1efcdd3e9bdcdb3adf259ba13f19a2dfb2559e
h3nnn4n <uber.renan@gmail.com>
2017-03-07 23:51:42 +0000

    It kinda works

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 extern crate rand;
-use rand::Rng;
 use rand::distributions::{IndependentSample, Range};
 
 #[derive(Clone, Copy)]
@@ -16,7 +15,7 @@ fn main () {
     let initial_temperature:f64 = 100.0;
     let final_temperature:f64 = 0.0;
 
-    let iterations:i32 = 1000;
+    let iterations:i32 = 1_000_000;
 
     let mut solution:Point = Point{x1:0.2, x2:5.1};
 
@@ -36,14 +35,15 @@ fn main () {
         let candidate_energy = objective_function(candidate);
         let delta_t = candidate_energy - current_energy;
 
-        if candidate_energy > current_energy {
+        if candidate_energy < current_energy {
             solution = candidate;
         } else if (-delta_t / temp).exp() > between.ind_sample(&mut rng) {
-            println!("WOW");
             solution = candidate;
         }
-
-        println!("fx({0} {1}) = {2}", solution.x1, solution.x2, objective_function(solution));
+        //println!("fx({0} {1}) = {2}", solution.x1, solution.x2, objective_function(solution));
+        //println!("{3} {0} {1} {2}", solution.x1, solution.x2, objective_function(solution), iter);
     }
+
+    println!("fx({0} {1}) = {2}", solution.x1, solution.x2, objective_function(solution));
 }
 
